34e83178574fec3b51c0978c1bf5d561567f5cf6
qzs3mda <114536204+qzs3mda@users.noreply.github.com>
2022-11-04 08:41:44 -0300

    Create InicioSesionController.cs

---
+++ SyncroCoder/SyncroCoder/Controllers/InicioSesionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SyncroCoder.Models;
+using SyncroCoder.Repository;
+using System.Reflection.PortableExecutable;
+
+namespace SyncroCoder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InicioSesionController : ControllerBase
+    {
+        [HttpGet("{nombreUsuario}/{contraseña}/")]
+
+        public List<Usuario> InicioSesionAPI(string nombreUsuario, string contraseña)
+        {
+            try
+            {
+                return ADO_Usuario.Login(nombreUsuario, contraseña);
+            }
+            catch (Exception)
+            {
+                return new List<Usuario>();
+            }
+            
+        }
+    }
+}
